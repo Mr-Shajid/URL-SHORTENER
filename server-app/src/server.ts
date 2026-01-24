5f7f323b17661ae6d651b0b2dbef1fb3770c3de3
Mr-Shajid <mdshahriarchyshajid.cse@gmail.com>
2026-01-25 05:54:54 +0600

    enabled cors

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -9,6 +9,13 @@ connectDb();
 const port = process.env.PORT || 5001;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+app.use(cors( {
+    origin : "http://localhost:3000",
+    credentials: true,
+}));
+
 
 app.get("/",(req,res)=> {
     res.send("Hello World");
